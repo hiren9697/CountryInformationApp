1a7db626daea2e3c0bc41e00014951583876fec4
Hiren Fadadu <hirenfaldu9697@gmail.com>
2023-04-21 07:52:47 +0530

    Initial commit

---
+++ CountryInformationApp/CountryInformationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryInformationAppApp.swift
+//  CountryInformationApp
+//
+//  Created by Hirenkumar Fadadu on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CountryInformationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
